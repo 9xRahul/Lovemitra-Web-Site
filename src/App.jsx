e2a8c310e56b987658250b203db3a8309ce7ac65
9xrahul <meetmeerahul@gmail.com>
2026-04-28 20:58:16 +0530

    Switch to HashRouter for domain independence

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
@@ -7,7 +7,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router basename="/Lovemitra-Web-Site">
+    <Router>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
